93d403bc77c56fbb6ca25daa3719e7e012781957
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-21 11:44:17 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,10 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-      if @post.valid?
-        @post.save
+    if @post.save
     redirect_to post_path(@post)
+    else 
+      render :edit
       end
   end
 
