112747c5e16233fad10d0678ead4dc749609dda3
nbaugh1 <nbaugh1@gmail.com>
2019-08-22 14:41:33 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_12_09_004521) do
-
+ActiveRecord::Schema.define(version: 20151209004521) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -26,5 +25,4 @@ ActiveRecord::Schema.define(version: 2015_12_09_004521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
